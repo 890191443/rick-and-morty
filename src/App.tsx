ea0aba08c9af6b2f3be291b2f8bb1e382787d6a3
890191443 <somaye.safavi.16@gmail.com>
2023-04-10 23:34:03 +0330

    routes and not found page

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,23 @@ import RickAndMorty from "./pages/rick-and-morty/RickAndMorty";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import NotFound from "./pages/not-found/NotFound";
 
 function App() {
   return (
     <>
-      <CssBaseline />
-      <Container fixed>
-        <Box sx={{ bgcolor: "#FFF", height: "100vh", width: "100%" }}>
-          <RickAndMorty />
-        </Box>
-      </Container>
+      <Router>
+        <CssBaseline />
+        <Container fixed>
+          <Box sx={{ bgcolor: "#FFF", height: "100vh", width: "100%" }}>
+            <Routes>
+              <Route path="/" element={<RickAndMorty />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Box>
+        </Container>
+      </Router>
     </>
   );
 }
